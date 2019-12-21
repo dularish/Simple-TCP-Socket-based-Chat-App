e0d2374f94182f2a532902a0467b4eb62823d502
dularish <dularish1993@gmail.com>
2019-12-21 09:25:48 +0530

    Basic working server client app

---
+++ SimpleClientApp/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace SimpleClientApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TcpClient tcpClient = new TcpClient(Dns.GetHostName(), 2060);
+
+            try
+            {
+                NetworkStream networkStream = tcpClient.GetStream();
+                StreamWriter streamWriter = new StreamWriter(networkStream) { AutoFlush = true };
+                streamWriter.Write("Hello from client");
+                networkStream.Close();
+            }
+            finally
+            {
+                tcpClient.Close();
+            }
+
+        }
+
+        private static string getLocalIPAddress()
+        {
+            string domainName = Dns.GetHostName();
+
+            IPAddress iPAddress = null;
+
+            foreach (var addressItem in Dns.GetHostEntry(domainName).AddressList)
+            {
+                Console.WriteLine("{0}/{1}", domainName, addressItem);
+                if (addressItem.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    iPAddress = addressItem;
+                    break;
+                }
+            }
+
+            return iPAddress.ToString();
+        }
+    }
+}
--- SocketServerApp/Program.cs
+++ SocketServerApp/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +13,49 @@ namespace SocketServerApp
     {
         static void Main(string[] args)
         {
+            string domainName = Dns.GetHostName();
+
+            IPAddress iPAddress = null;
+
+            foreach (var addressItem in Dns.GetHostEntry(domainName).AddressList)
+            {
+                Console.WriteLine("{0}/{1}", domainName, addressItem);
+                if (addressItem.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    iPAddress = addressItem;
+                    break;
+                }
+            }
+
+            if(iPAddress != null)
+            {
+                TcpListener tcpListener = new TcpListener(iPAddress,2060);
+
+                tcpListener.Start();
+
+                while (true)
+                {
+                    Console.WriteLine("Listening to socket...");
+                    Socket socket = tcpListener.AcceptSocket();
+                    Console.WriteLine("Socket accepted. Processing further");
+                    NetworkStream networkStream = new NetworkStream(socket);
+
+                    StreamReader reader = new StreamReader(networkStream);
+                    StreamWriter writer = new StreamWriter(networkStream);
+                    string line;
+                    while(!string.IsNullOrEmpty(line = reader.ReadLine())){
+                        Console.WriteLine(line);
+                    }
+
+                    networkStream.Close();
+
+                    socket.Close();
+                }
+            }
+
+            
+
+            Console.ReadKey();
         }
     }
 }
